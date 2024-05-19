4db9ec29fb559550ae623818ed8c3a85262ebd1d
FullstackDelOeste <142056061+FullstackDelOeste@users.noreply.github.com>
2024-05-19 17:21:32 -0300

    se agrego un textarea a la pagina contactos y un message

--- proyectoDjango/laBiblioteca/forms.py
+++ proyectoDjango/laBiblioteca/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 class AltaLectoresForm(forms.Form):
     nombre = forms.CharField(label="Nombre", required=True)
@@ -9,6 +10,35 @@ class AltaLectoresForm(forms.Form):
     repetirContraseña = forms.CharField(label="Repita su Contraseña", required=True)
     direccion = forms.CharField(label="Dirección", required=True)
     
+    def clean_nombre(self):
+        if not self.cleaned_data["nombre"].isalpha():
+            raise ValidationError("El nombre solo puede estar compuesto por letras")
+        
+        return self.cleaned_data["nombre"]
+    
+    def clean_apellido(self):
+        if not self.cleaned_data["apellido"].isalpha():
+            raise ValidationError("El apellido solo puede estar compuesto por letras")
+        
+        return self.cleaned_data["apellido"]
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        nombre = cleaned_data.get("nombre")
+        apellido = cleaned_data.get("apellido")
+        contraseña = cleaned_data.get("contraseña")
+        contraseña2 = cleaned_data.get("repetirContraseña")
+        
+        if nombre == "nombre" and apellido == "apellido":
+            raise ValidationError("El usuario ya existe")
+        
+        if self.cleaned_data["dni"] < 1000000:
+            raise ValidationError("ElDNI debe contener almenos 7 digitos")
+        
+        if not contraseña == contraseña2:
+            raise ValidationError("Las contraseñas deben ser iguales")
+
+        return self.cleaned_data    
     
     
     
@@ -23,5 +53,5 @@ class IngresoLectoresForm(forms.Form):
 class ContactosForm(forms.Form):
     nombre = forms.CharField(label="Nombre", required=True)
     email = forms.EmailField(label="Email", required=True)
-    mensaje = forms.CharField(label="Mensaje", required=True)
+    mensaje = forms.CharField(label="Mensaje", required=True,widget=forms.Textarea(attrs={'class': 'textarea_contacto'}))
     recibir_noticias = forms.BooleanField(label="Suscribirse a noticias", required=False)
--- proyectoDjango/laBiblioteca/views.py
+++ proyectoDjango/laBiblioteca/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from . import forms
 from .forms import AltaLectoresForm, IngresoLectoresForm
+from django.contrib import messages
 
 def index(request):
     return render(request, 'laBiblioteca/index.html')
@@ -11,14 +12,19 @@ def registro(request):
     if request.method =="GET":
         contexto['alta_lector_form'] = forms.AltaLectoresForm()
     else: #asumo que es un POST
-        contexto['alta_alumno_form'] = forms.AltaLectoresForm(request.POST)
+        form = AltaLectoresForm(request.POST)
+        contexto['alta_lector_form'] = forms.AltaLectoresForm(request.POST)
         #validar el form
-        #si el form es correcto
-        #si el form es correcto lo reirijo
+        if form.is_valid():
+            #si el form es correcto
+            #si el form es correcto lo reirijo
+            messages.success(request, '¡Te has dado de alta con éxito!')
+            print(request.POST)
+            
+            return redirect('ingresar')
         #si el form contiene errores, envio un mensaje de error
         #si todo esta OK; hago un comit en la base de datos
-        redirect('index')
-        print(request.POST)
+       
         
     return render(request, 'laBiblioteca/registro.html', contexto)   
     
@@ -31,14 +37,23 @@ def ingresar(request):
     if request.method =="GET":
         contexto['ingreso_lector_form'] = forms.IngresoLectoresForm()
     else: #asumo que es un POST
+        form = AltaLectoresForm(request.POST)
         contexto['ingreso_lector_form'] = forms.IngresoLectoresForm(request.POST)
         #validar el form
+        #validar el form
+        # if form.is_valid():
+        #     #si el form es correcto
+        #     #si el form es correcto lo reirijo
+        #     print(request.POST)
+            
+        #     return redirect('panel_usuario')
         #si el form es correcto
         #si el form es correcto lo reirijo
         #si el form contiene errores, envio un mensaje de error
         #si todo esta OK; hago un comit en la base de datos
-        redirect('index')
         print(request.POST)
+        return redirect('panel_usuario')
+        
     
     
     return render(request, 'laBiblioteca/ingresar.html', contexto)
@@ -52,8 +67,16 @@ def contactos(request):
     if request.method =="GET":
         contexto['contactos_form'] = forms.ContactosForm()
     else: #asumo que es un POST
+        form = AltaLectoresForm(request.POST)
         contexto['contactos_form'] = forms.ContactosForm(request.POST)
         #validar el form
+        #validar el form
+        if form.is_valid():
+            #si el form es correcto
+            #si el form es correcto lo reirijo
+            print(request.POST)
+            
+            redirect('index')
         #si el form es correcto
         #si el form es correcto lo reirijo
         #si el form contiene errores, envio un mensaje de error
