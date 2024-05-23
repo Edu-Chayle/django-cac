7190ea65b8af080aa954a98dad7a4e8698c925e3
Edu-Chayle <edu.chayle@gmail.com>
2024-05-22 22:35:34 -0300

    Se agregó PerfilForm en forms.py

--- proyectoDjango/laBiblioteca/forms.py
+++ proyectoDjango/laBiblioteca/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+import re
 
 class AltaLectoresForm(forms.Form):
     nombre = forms.CharField(label="Nombre", required=True)
@@ -55,3 +56,46 @@ class ContactosForm(forms.Form):
     email = forms.EmailField(label="Email", required=True)
     mensaje = forms.CharField(label="Mensaje", required=True,widget=forms.Textarea(attrs={'class': 'textarea_contacto'}))
     recibir_noticias = forms.BooleanField(label="Suscribirse a noticias", required=False)
+
+class PerfilForm(forms.Form):
+    nombre = forms.CharField(max_length=50, label="Nombre", required=True)
+    apellido = forms.CharField(max_length=50, label="Apellido", required=True)
+    email = forms.EmailField(label="Email", required=True)
+    contrasenia = forms.CharField(min_length=8, max_length=162, label="Contraseña", required=True, 
+                                  widget=forms.PasswordInput, 
+                                  help_text="Mínimo 8 caracteres, con al menos una letra minúscula, una letra \
+                                             mayúscula y un número.")
+    descripcion = forms.CharField(max_length=255, label="Descripción", required=False)
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+
+        if not nombre.isalpha():
+            raise forms.ValidationError("El nombre solo debe contener letras.")
+
+        return nombre
+
+    def clean_apellido(self):
+        apellido = self.cleaned_data.get('apellido')
+
+        if not apellido.isalpha():
+            raise forms.ValidationError("El apellido solo debe contener letras.")
+
+        return apellido
+
+    """def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if email and Usuario.objects.filter(email=email).exists():
+            raise forms.ValidationError("El email ya se encuentra registrado.")
+
+        return email"""
+
+    def clean_contrasenia(self):
+        re_contrasenia = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{,}$"
+        contrasenia = self.cleaned_data.get('contrasenia')
+
+        if not re.match(re_contrasenia, contrasenia):
+            raise forms.ValidationError("La contraseña no es válida.")
+
+        return contrasenia
