700b13b8e64f7bd5da1d1297839dffd19023c0eb
Edu-Chayle <edu.chayle@gmail.com>
2024-06-16 17:33:24 -0300

    Se agregó cart.py y carrito.html

--- proyectoDjango/laBiblioteca/urls.py
+++ proyectoDjango/laBiblioteca/urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('registro', views.registro, name = 'registro'),
     path('ingresar', views.ingresar, name = 'ingresar'),
-    path('catalogo', views.catalogo, name = 'catalogo'),
+    path('catalogo', views.CatalogoLibros.as_view(), name ='catalogo'),
+    path('carrito/agregar/<int:libro_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
+    path('carrito/quitar/<int:libro_id>/', views.quitar_del_carrito, name='quitar_del_carrito'),
+    path('carrito/vaciar', views.clear_cart, name='clear_cart'),
     path('contactos', views.contactos, name = 'contactos'),
     path('panel_usuario', views.panel_usuario, name = 'panel_usuario'),
     path('perfil', views.perfil, name='perfil'),
+    path('carrito', views.carrito, name='carrito'),
 ]
--- proyectoDjango/laBiblioteca/views.py
+++ proyectoDjango/laBiblioteca/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from django.views.generic.list import ListView
 from . import forms
+from .cart import Cart
 from .forms import AltaLectoresForm, IngresoLectoresForm
+from .models import Cliente, Libro, Venta, VentaLibro
 from django.contrib import messages
 
 def index(request):
@@ -20,17 +23,12 @@ def registro(request):
             #si el form es correcto lo reirijo
             messages.success(request, '¡Te has dado de alta con éxito!')
             print(request.POST)
-            
+
             return redirect('ingresar')
         #si el form contiene errores, envio un mensaje de error
         #si todo esta OK; hago un comit en la base de datos
-       
-        
-    return render(request, 'laBiblioteca/registro.html', contexto)   
-    
-    
-    
 
+    return render(request, 'laBiblioteca/registro.html', contexto)   
 
 def ingresar(request):
     contexto = {}
@@ -53,14 +51,66 @@ def ingresar(request):
         #si todo esta OK; hago un comit en la base de datos
         print(request.POST)
         return redirect('panel_usuario')
-        
-    
-    
+
     return render(request, 'laBiblioteca/ingresar.html', contexto)
 
+class CatalogoLibros(ListView):
+    model = Libro
+    context_object_name = 'libros'
+    template_name = 'laBiblioteca/catalogo.html'
+    ordering = ['titulo']
 
-def catalogo(request):
-    return render(request, 'laBiblioteca/catalogo.html')
+def agregar_al_carrito(request, libro_id):
+    libro = get_object_or_404(Libro, id = libro_id)
+    cart = Cart(request)
+
+    cart.add_to_cart(libro)
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+def quitar_del_carrito(request, libro_id):
+    cart = Cart(request)
+
+    cart.remove_from_cart(libro_id)
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+def clear_cart(request):
+    cart = Cart(request)
+
+    cart.clear_cart()
+
+    return redirect('carrito')
+
+def carrito(request):
+    cart = Cart(request)
+    total_price = cart.get_total_price()
+
+    return render(request, 'laBiblioteca/carrito.html', {'cart': cart, 'total_price': total_price})
+
+def venta(request):
+    cliente = get_object_or_404(Cliente, email=request.user.email)
+    cart = Cart(request)
+
+    nueva_venta = Venta.objects.create(
+        factura="FACT" + str(timezone.now().timestamp()).replace('.', ''),
+        cliente=cliente,
+        monto_total=cart.get_total_price()
+    )
+
+    for libro_id, item in cart.cart.items():
+        libro = get_object_or_404(Libro, id=libro_id)
+
+        VentaLibro.objects.create(
+            venta=nueva_venta,
+            libro=libro,
+            cantidad=item['cantidad'],
+            precio_unitario=item['precio']
+        )
+
+    cart.clear_cart()
+
+    return redirect('index')
 
 def contactos(request):
     contexto = {}
@@ -75,7 +125,7 @@ def contactos(request):
             #si el form es correcto
             #si el form es correcto lo reirijo
             print(request.POST)
-            
+
             redirect('index')
         #si el form es correcto
         #si el form es correcto lo reirijo
@@ -83,7 +133,7 @@ def contactos(request):
         #si todo esta OK; hago un comit en la base de datos
         redirect('index')
         print(request.POST)
-        
+
     return render(request, 'laBiblioteca/contactos.html', contexto)
 
 def panel_usuario(request):
